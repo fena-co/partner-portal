f7b289ae99654baba958d83f24cc50e6a815f7de
fominmaksim <fomin.max.13@gmail.com>
2022-02-18 12:09:23 +0200

    feat: installed and configured eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-function App() {
+const App: React.FunctionComponent = () => {
   return <div className="App">hello</div>;
-}
+};
 
 export default App;
